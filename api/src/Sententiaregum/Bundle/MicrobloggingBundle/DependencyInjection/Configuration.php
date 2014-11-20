b25b51279aedd61bca70bc2dd794f6e6400c0f0e
Ma27 <ma27-se@hotmail.com>
2014-11-20 23:11:37 +0100

    fix nug in microblogging tree builder

--- api/src/Sententiaregum/Bundle/MicrobloggingBundle/DependencyInjection/Configuration.php
+++ api/src/Sententiaregum/Bundle/MicrobloggingBundle/DependencyInjection/Configuration.php
@@ -18,11 +18,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        #$rootNode = $treeBuilder->root('sententiaregum_microblogging');
+        $rootNode = $treeBuilder->root('sententiaregum_microblogging');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        /*$rootNode
+            ->children()
+
+            ->end();*/
 
         return $treeBuilder;
     }
