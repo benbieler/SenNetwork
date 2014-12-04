978e01ff23767d2e80fcf637c4f8ce9fc29817f1
Ma27 <maximilian.bosch.27@gmail.com>
2014-12-04 21:53:11 +0100

    fixed #17

---
+++ Rakefile.rb
@@ -0,0 +1,48 @@
+task :installFrontend do
+    puts "Installing node.js dependencies"
+    `npm install`
+    puts "Installing Bower dependencies"
+    `bower install`
+    puts "Running grunt tasks"
+    `grunt build-production`
+end
+
+task :installBackend do
+    `cd api`
+    puts "Installing Composer packages and running symfony deploy tasks"
+    `composer install`
+    puts "Checking Symfony 2 requirements"
+    `php app/check.php`
+    puts "Creating Doctrine Schema"
+    `php app/console doctrine:migrations:migrate --no-interaction`
+    puts "Creating admin user"
+    `php app/console sententiaregum:user:create-admin --name=root --password=sen-unsage-password --email=root@example.org`
+    puts "Flush redis"
+    `php app/console redis:flush`
+    `cd ..`
+end
+
+task :testBehat do
+    puts "Processing behat tests"
+    `cd api`
+    `bin/behat`
+    `cd ..`
+end
+
+task :testPHPSpecs do
+    puts "Processing specs"
+    `cd api`
+    `bin/phpspec run`
+    `cd ..`
+end
+
+task :prepareTravisCI do
+    puts "Run commands to prepare travis ci"
+    `printf "\n" | pecl install imagick`
+    `mysql -e "CREATE DATABASE IF NOT EXISTS symfony;" -u root`
+    `echo "use mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user = 'root';\nFLUSH PRIVILEGES;\n" | mysql -u root`
+end
+
+task :test => [:testPHPSpecs, :testBehat]
+task :default => [:installFrontend, :installBackend]
+task :travis => [:prepareTravisCI, :default]
