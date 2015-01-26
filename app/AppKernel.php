4c69903fb7385c2eb1db56433021256a3140bf80
Ma27 <maximilian.bosch.27@gmail.com>
2015-01-26 23:06:50 +0100

    add user bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Sententiaregum\Bundle\UserBundle\SententiaregumUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
---
+++ src/Sententiaregum/Bundle/UserBundle/SententiaregumUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sententiaregum\Bundle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SententiaregumUserBundle extends Bundle
+{
+}
