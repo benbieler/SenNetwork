37d83a7228b372ab6d815951a7361dae507e0235
Ma27 <maximilian.bosch.27@gmail.com>
2015-02-02 23:01:28 +0100

    implement auth controller

---
+++ src/Sententiaregum/Bundle/UserBundle/Controller/AuthenticationController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sententiaregum\Bundle\UserBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sententiaregum\Bridge\User\DTO\AuthDTO;
+use Sententiaregum\Bundle\UserBundle\Exception\FailedAuthenticationException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * This controller handles the authentication requests
+ */
+class AuthenticationController extends Controller
+{
+    /**
+     * Checks the user's credentials and generates an api key
+     *
+     * @param Request $request
+     *
+     * @return mixed[]
+     *
+     * @throws FailedAuthenticationException If the auth process has failed
+     *
+     * @Route("/request-token.{_format}", name="sen_user_auth")
+     * @Method({"GET", "POST"})
+     * @View()
+     * @ApiDoc(
+     *     description="This action generates api keys if the user is able to authenticate himself with credentials"
+     *     statusCodes={
+     *         200="The action was successful",
+     *         401="The auth processor denies access"
+     *     }
+     * )
+     */
+    public function authenticateTokenAction(Request $request)
+    {
+        $credentials = new AuthDTO(null, null);
+        $form        = $this->createForm('sen.user.form.auth', $credentials);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            /** @var \Sententiaregum\Bridge\User\Service\AuthenticationInterface $authService */
+            $authService = $this->get('sen.user.auth.api_key_auth');
+            $authService->setRequesterIp($request->getClientIp());
+
+            $result = $authService->signIn($credentials);
+            if ($result->isFailed()) {
+                throw new FailedAuthenticationException($result->getFailReason());
+            }
+
+            $entityManager->flush();
+            return ['apiKey' => $result->getUser()->getToken()->getApiKey()];
+        }
+    }
+}
