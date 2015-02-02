f527491a75fd6fefe6a7dc44a42064eec70ac949
Ma27 <maximilian.bosch.27@gmail.com>
2015-02-02 18:43:35 +0100

    enable code coverage scrutinizer

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,6 @@
 desc "This task tests the application"
 task :test do
-    sh %{bin/phpunit -c app}
+    sh %{bin/phpunit -c app --coverage-clover=coverage.clover}
 end
 
 desc "This task purges if the software should be uninstalled"
@@ -15,4 +15,9 @@ task :prepareCI do
     sh %{echo "use mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user = 'root';\nFLUSH PRIVILEGES;\n" | mysql -u root}
     sh %{npm install -g bower}
     sh %{composer install}
+end
+
+task :uninstallCI => "uninstall" do
+    sh %{wget https://scrutinizer-ci.com/ocular.phar}
+    sh %{php ocular.phar code-coverage:upload --format=php-clover coverage.clover}
 end
